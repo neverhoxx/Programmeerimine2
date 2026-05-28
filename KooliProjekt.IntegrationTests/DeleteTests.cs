57c5f1ee4d345f2fef658083238d0e42e58404d3
yehor <yehor.kiiakh@gmail.com>
2026-05-28 12:56:56 +0300

    Add DeleteTests class for item deletion testing

---
+++ KooliProjekt.IntegrationTests/DeleteTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace KooliProjekt.IntegrationTests;
+
+public class DeleteTests
+{
+    [Fact]
+    public async Task Delete_Should_Remove_Item()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+
+        Assert.True(true);
+    }
+}
